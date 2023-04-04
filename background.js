056d395fded3a67efb296757c1a893107a372c0d
שלמה <78599753+ShlomoCode@users.noreply.github.com>
2023-04-04 17:11:25 +0300

    return if tab url starts with 'chrome://'

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-chrome.action.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener((tab) => {
+    if (tab.url?.startsWith('chrome://')) return undefined;
     chrome.scripting.executeScript({
-        target: {tabId: tab.id, allFrames: true},
+        target: { tabId: tab.id, allFrames: true },
         files: ['content.js'],
     });
 });
